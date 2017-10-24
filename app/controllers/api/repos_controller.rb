b35d9b4c728a0b1a289c1a192bc69442dc261511
Tim Dorr <timdorr@timdorr.com>
2017-10-24 19:52:34 -0400

    Cache the github call.

--- app/controllers/api/repos_controller.rb
+++ app/controllers/api/repos_controller.rb
@@ -7,11 +7,13 @@ module Api
     private
 
     def repos
-      current_user&.github&.repositories
-        &.group_by { |r| r.owner.login }
-        &.transform_values do |repos|
-          repos.map{ |r| r.to_h.slice(:id, :name, :description, :html_url, :private, :fork) }
-        end
+      Rails.cache.fetch("github/repos/#{current_user.id}", expires_in: 1.hour) do
+        current_user&.github&.repositories
+          &.group_by { |r| r.owner.login }
+          &.transform_values do |repos|
+            repos.map{ |r| r.to_h.slice(:id, :name, :description, :html_url, :private, :fork) }
+          end
+      end
     end
   end
 end

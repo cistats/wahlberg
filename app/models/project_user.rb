19b56a488f56b3c024731c2d5718d4e98e41fda8
Tim Dorr <timdorr@timdorr.com>
2017-10-24 23:22:07 -0400

    Make syncing more robust.
    Delete removed repos. Use less queries.

--- app/models/project_user.rb
+++ app/models/project_user.rb
@@ -2,15 +2,24 @@ class ProjectUser < ApplicationRecord
   belongs_to :project
   belongs_to :user
 
-  # @param [User] user
   def self.sync_for_user(user)
-    user.repositories.each do |repo|
-      project = Project.create_with(
+    return if user.github_synced_at > 1.minute.ago
+
+    visible_projects = user.repositories.map do |repo|
+      Project.create_with(
         org: repo.owner.login,
         repo: repo.name
       ).find_or_create_by(github_id: repo.id)
+    end
 
-      project.users << user unless project.users.include?(user)
+    user_projects = user.projects
+
+    user_projects.each do |project|
+      user_projects.delete(project) unless visible_projects.include?(project)
+    end
+
+    visible_projects.each do |project|
+      project.users << user unless user_projects.include?(project)
     end
 
     user.update(github_synced_at: Time.current)

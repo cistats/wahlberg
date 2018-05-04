79bea7f181f90c14267d95fde8b3eb9f79ef4599
Tim Dorr <timdorr@timdorr.com>
2018-05-03 22:22:58 -0400

    Ensure the handle is filled.

--- app/models/team.rb
+++ app/models/team.rb
@@ -3,4 +3,10 @@ class Team < ApplicationRecord
   has_many :users, through: :team_members
 
   has_many :projects
+
+  before_create :ensure_handle
+
+  def ensure_handle
+    self.handle = name.parameterize if handle.blank?
+  end
 end
--- spec/models/team_spec.rb
+++ spec/models/team_spec.rb
@@ -1,2 +1,10 @@
 RSpec.describe Team do
+  subject { FactoryBot.build(:team) }
+
+  it 'ensures it has a handle' do
+    subject.handle = nil
+    subject.run_callbacks(:create)
+
+    expect(subject.handle).to eq(subject.name.parameterize)
+  end
 end
